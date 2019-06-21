8d5e54334445c8b1ce079cf5b35087519b39f479
JensPfeifle <jens@pfeifle.tech>
2019-06-21 17:12:37 +0200

    working solver

--- uebung03/solve_thomas.f90
+++ uebung03/solve_thomas.f90
@@ -21,18 +21,36 @@ double precision, dimension(2) :: r
 
 ! x
 double precision, dimension(3) :: x
+double precision, dimension(3) :: y
 
 ! Given LGS
-!   [6 -1  0]   |14|
+!   [6 -2  0]   |14|
 !   [9 -1  1] = |21|
 !   [0 -7  5]   |17|
 
 a = [9, -7]
 b = [6, -1, 5]
-c = [-1, 1]
+c = [-2, 1]
 d = [14,21,17]
 
+write(*,*) 'original'
+write(*,*) "a", a
+write(*,*) "b", b
+write(*,*) "c", c
+
 call ludecomp(a,b,c,l,m,r)
+
+write(*,*) 'decomp'
+write(*,*) "l", l
+write(*,*) "m", m
+write(*,*) "r", r
+
+call forwardssubs(l,d,y)
+call backwardssubs(m,r,x)
+
+write(*,*) 'solved'
+write(*,*) 'y', y
+write(*,*) 'x', x
 
 contains
 
@@ -42,22 +60,57 @@ subroutine ludecomp(a,b,c, l, m, r)
     double precision, intent(in) :: a(:)
     double precision, intent(in) :: b(:)
     double precision, intent(in) :: c(:)
-
     double precision, intent(out) :: l(:)
     double precision, intent(out) :: m(:)
     double precision, intent(out) :: r(:)
+    integer :: n, i
 
-    write(*,*) "a", a
-    write(*,*) "b", b
-    write(*,*) "c", c
-
-
-    write(*,*) "l", l
-    write(*,*) "m", m
-    write(*,*) "r", r
-
+    n = size(b)
+    m(1) = b(1)
+    do i=1,n-1
+        r(i) = c(i)
+        l(i) = a(i)/m(i)
+        m(i+1) = b(i+1) - l(i)*r(i)
+    end do
 
 end subroutine ludecomp
+
+subroutine forwardssubs(l,d,y)
+    ! solve Ly=d for y
+    implicit none
+    double precision, intent(in) :: l(:)
+    double precision, intent(in) :: d(:)
+    double precision, intent(out) :: y(:)
+    integer :: n, i
+
+    n=size(d)
+
+    y(1)=d(1)
+
+    do i=2,n
+        y(i)=d(i)-l(i-1)*y(i-1)
+    end do
+
+end subroutine forwardssubs
+
+subroutine backwardssubs(m,r,x)
+    ! solve Ux=y for x
+    implicit none
+    double precision, intent(in) :: m(:)
+    double precision, intent(in) :: r(:)
+    double precision, intent(out) :: x(:)
+    integer :: n, i
+
+    n=size(x)
+
+    x(n)=y(n)/m(n)
+
+    do i=n-1,1, -1
+        write(*,*) 'i',i
+        x(i)=(y(i)-r(i)*x(i+1))/m(i)
+    end do
+
+end subroutine backwardssubs
 
 end program thomas
 
