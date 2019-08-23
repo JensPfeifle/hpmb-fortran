86fc2fa3d93b31f64da717b35dc92c534c2489da
jenspfeifle <jens@pfeifle.tech>
2019-08-23 12:53:50 +0200

    increase performance of animation, add suplot

--- preufung/viewer.py
+++ preufung/viewer.py
@@ -52,52 +52,67 @@ def init():
     """
     animation initialization function
     """
+
     for patch in ballpatches:
         ax.add_patch(patch)
     return ballpatches
 
-def animate(frame):
+def animate(framedata):
     """
     animation update function
     """
-    framedata = extract_frame_data(frame)
     framenum, t, kinetic, balldata = framedata
     for i in range(len(balldata)):
         patch = ballpatches[i]
         x = balldata[i+1].xpos
         y = balldata[i+1].ypos
         patch.center = (x, y)
-
     return ballpatches # only return updated?
 
 def anim_frames(frame_iterator, interval):
     # interval between animation frames [ms]
-    frame_deltat = 0.0001 # [s]
+    frame_deltat = 0.00001 # [s]
     step = int(interval/1000/frame_deltat)
     return itertools.islice(frame_iterator,0,None,step)
 
 if __name__ == "__main__":
     
     # read data
-    with open('pool.out','r') as f:
+    with open('/home/jens/git/hpmb-fortran/preufung/pool.out','r') as f:
         lines = f.read().strip().split('\n')
     # process data
     frames = split_into_frames(lines)
 
     # setup canvas
-    fig, ax = plt.subplots()
+    fig, (ax,ax2) = plt.subplots(2,1)
+    #fig, ax = plt.subplots()
+
+
+
+    animationframes = list(anim_frames(frames, 16))
+
+    framedata = [extract_frame_data(f) for f in animationframes]
+
+
+    fnum = [f[0] for f in framedata]
+    ekin = [f[2] for f in framedata]
+
+    #ax2.set_xlim(left=0)
+    #ax2.set_ylim(bottom=0)
+    ax2.plot(fnum,ekin,label='ekin')
+    ax2.legend()
+
     ax.set_xlim(0,111.76)
     ax.set_ylim(0,223.52)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
     ax.set_aspect('equal')
     # ballpatches is global variable
-    for i in range(3): # hardcoded number of balls!
-        ballpatches.append(plt.Circle((0,0),5.7/2))
-
-    aframes = list(anim_frames(frames, 16))
-
+    for i in range(16): # hardcoded max number of balls!
+        ballpatches.append(plt.Circle((-10,0),5.7/2))
     anim = animation.FuncAnimation(fig, animate, 
                                 init_func=init, 
-                                frames=aframes, 
+                                frames=framedata, 
                                 interval=16,
                                 repeat=True,
                                 blit=True)
