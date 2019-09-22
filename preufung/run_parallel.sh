b88eb3026c1c49d74116f191c3303301302888da
JensPfeifle <jens@pfeifle.tech>
2019-09-22 15:30:16 +0200

    script to start a parallel run

---
+++ preufung/run_parallel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "How many threads?" threads
+export OMP_NUM_THREADS=$threads
+echo "Starting ./main with $threads threads..."
+time (./main > pool.out)
+echo "View results with 'python viewer.py?'"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) python viewer.py ; break;;
+        No ) exit;;
+    esac
+done
