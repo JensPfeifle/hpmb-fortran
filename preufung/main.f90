1be8d1dd3060444e9d0c42381253d7cb7d1845c6
JensPfeifle <jens@pfeifle.tech>
2019-09-22 10:16:10 +0200

    add second integration scheme

--- preufung/main.f90
+++ preufung/main.f90
@@ -2,17 +2,20 @@ program main
     use globals
     use initial
     use forces
+    use integration
     implicit none
 
-    real(dp), allocatable, dimension(:) :: x,y,vx,vy,ax,ay,fx,fy
+    real(dp), allocatable, dimension(:) :: x,y,vx,vy,fx,fy
     real(dp)                            :: t, dt, t_total
     real(dp)                            :: width_mm, height_mm, qball_vel
     integer                             :: i, frame
     real(dp)                            :: vtotal2, ekin
-    integer                             :: numballs
+    integer                             :: numballs, integration_scheme
 
-    namelist /INPUT/ numballs,width_mm,height_mm,t_total,dt,qball_vel
-    
+    namelist /INPUT/ numballs,width_mm,height_mm,   &
+                     t_total,dt,integration_scheme, &
+                     qball_vel
+                     
     open( UNIT=10, file='input.dat' )
     read( UNIT=10, NML=INPUT )
     close(10)
@@ -21,8 +24,6 @@ program main
              y(numballs), &
              vx(numballs), &
              vy(numballs), &
-             ax(numballs), &
-             ay(numballs), &
              fx(numballs), &
              fy(numballs)  )
 
@@ -34,40 +35,60 @@ program main
     call set_nballs(numballs)
     call initial_setup(x,y,vx,vy)
 
-    do while (t < t_total)
-        ! ausgabe
-        write(*,*) "FRAME", frame
-        write(*,*) t, ekin
-        do i=1,numballs
-            write(*,*) i, x(i), y(i), vx(i), vy(i)
+    select case(integration_scheme)
+    case(1) ! euler
+        do while (t < t_total)
+            call write_(frame,t,ekin,x,y,vx,vy)          ! ausgabe
+            call force(x,y,vx,vy,fx,fy,numballs)         ! bestimme kraefte
+            call positions_eul(x,y,vx,vy,dt)             ! bewege atome
+            call velocities_eul(vx,vy,fx,fy,dt)          ! aktualisiere geschwindigkeiten
+            ekin = ekin_(vx,vy)                          ! berechne kinetische energie
+            ! zeitschritt
+            frame = frame + 1
+            t = t + dt
         end do
-        write(*,*) "ENDF"
-        ! bestimme kraefte
-        call force(x,y,vx,vy,fx,fy,numballs)
-        ! bestimme beschleunigungen
-        do i=1,numballs
-            ax(i) = fx(i)/m
-            ay(i) = fy(i)/m
+    case(2) ! stoermer-verlet
+        call init_ver(numballs)                          ! f_old initialisieren
+        do while (t < t_total)
+            call write_(frame,t,ekin,x,y,vx,vy)          ! ausgabe
+            call force(x,y,vx,vy,fx,fy,numballs)         ! bestimme kraefte
+            call positions_ver(x,y,vx,vy,fx,fy,dt)       ! bewege atome
+            call velocities_ver(vx,vy,fx,fy,dt)          ! aktualisiere geschwindigkeiten
+            ekin = ekin_(vx,vy)                          ! berechne kinetische energie
+            ! zeitschritt
+            frame = frame + 1
+            t = t + dt
         end do
-        ! bewege atome
-        do i=1,numballs
-            x(i) = x(i) + vx(i)*dt + ax(i)*dt*dt/2_dp
-            y(i) = y(i) + vy(i)*dt + ay(i)*dt*dt/2_dp
-        end do
-        ! aktualisiere geschwindigkeiten
-        do i=1,numballs
-            vx(i) = vx(i) + ax(i)*dt/2_dp
-            vy(i) = vy(i) + ay(i)*dt/2_dp
-        end do
-        ! berechne kinetische energie
-        ekin = 0.
-        do i=1,numballs
-            vtotal2 = vx(i)**2 + vy(i)**2
-            ekin = ekin + 0.5_dp*m*vtotal2
-        end do
-        ! zeitschritt
-        frame = frame + 1
-        t = t + dt
-        end do
+    end select
+
+contains
+
+function ekin_(vx,vy) result(e)
+    real(dp), dimension(:), intent(in)  :: vx,vy
+    real(dp)                            :: e
+    e = 0.
+    do i=1,size(vx)
+        e = e + 0.5_dp*m*( vx(i)**2 + vy(i)**2 )
+    end do
+end function ekin_
+
+subroutine write_(f,t,ekin,x,y,vx,vy)
+    integer, intent(in)                 :: f
+    real(dp), intent(in)                :: t,ekin
+    real(dp), dimension(:), intent(in)  :: x,y
+    real(dp), dimension(:), intent(in)  :: vx,vy
+    integer                             :: i, numballs
+
+    numballs = size(x)
+    write(*,*) "FRAME", f
+    write(*,*) t, ekin
+    do i=1,numballs
+        write(*,*) i, x(i), y(i), vx(i), vy(i)
+    end do
+    write(*,*) "ENDF"
+
+
+    end subroutine write_
         
-    end program main
+end program main
+
