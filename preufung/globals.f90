2c252134f4f0e843d4ab048fb5254892713dcfb0
JensPfeifle <jens@pfeifle.tech>
2019-09-22 15:29:30 +0200

    add cutoff radius and ball radius to globals

--- preufung/globals.f90
+++ preufung/globals.f90
@@ -4,6 +4,8 @@ module globals
     integer, parameter :: dp=kind(1.d0)
     
     real, protected     :: g = 980.665 ! [cm/s2]
+    real, protected     :: r = 2.85 ! radius of balls
+    real, protected     :: rcut = 2*2.85 ! cutoff for interactions
     real, protected     :: m = 0.17 ! mass of balls [kg]
     real, protected     :: mu = 0.008 ! rolling resistance
 
